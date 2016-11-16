878903922175e8e1272455577004067cfac74964
abhay0609 <abhay0609@gmail.com>
2016-11-16 20:03:19 +0530

    edit solution

--- src/main/java/arrays/ScanningBothSidesAlgo.java
+++ src/main/java/arrays/ScanningBothSidesAlgo.java
@@ -13,11 +13,11 @@ public class ScanningBothSidesAlgo {
         int[] right = new int[arr.length];
         int water = 0;
         left[0] = arr[0];
-        for (int i = 1; i < arr.length; i++) left[i] = Math.max(left[i - 1], left[i]);
+        for (int i = 1; i < arr.length; i++) left[i] = Math.max(left[i - 1], arr[i]);
         right[arr.length - 1] = arr[arr.length - 1];
-        for (int i = arr.length - 2; i >= 0; i--) right[i] = Math.max(right[i + 1], right[i]);
+        for (int i = arr.length - 2; i >= 0; i--) right[i] = Math.max(right[i + 1], arr[i]);
 
-        for (int i = 0; i < arr.length; i++) water = Math.min(left[i], right[i]) - arr[i];
+        for (int i = 0; i < arr.length; i++) water += Math.min(left[i], right[i]) - arr[i];
         return water;
     }
 
@@ -30,6 +30,10 @@ public class ScanningBothSidesAlgo {
     * 2. Children with a higher rating get more candies than their neighbors.
     * */
 
+    public static void main(String[] args) {
 
+        int[] arr = {0,1,0,2,1,0,1,3,2,1,2,1};
+        System.out.print(findWater(arr));
+    }
 
 }
